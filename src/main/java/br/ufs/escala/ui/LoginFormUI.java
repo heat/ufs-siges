86fd9a09394dc3ab8259eefe0eba3a97cf5b4a91
unknown <aluno@DCOMP191.dcomp.ufs.br>
2013-03-25 21:58:03 -0300

    inclui regras de login e tela para login

--- src/main/java/br/ufs/escala/ui/LoginFormUI.java
+++ src/main/java/br/ufs/escala/ui/LoginFormUI.java
@@ -1,13 +1,36 @@
 package br.ufs.escala.ui;
 
 import br.ufs.escala.transfer.Usuario;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
+@ManagedBean
+@RequestScoped
 public class LoginFormUI {
-	public void login() {
 
-	}
+    private String usuario = "Onezino Gabriel Moreira";
+    private String senha;
 
-	private void sessionRegister(Usuario user) {
+    public String login() {
+        return "success";
+    }
 
-	}
+    private void sessionRegister(Usuario user) {
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
